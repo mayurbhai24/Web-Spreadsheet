f169e4cc32601dc703205a9220ad74b9926b1a2a
mayurbhai24 <55508847+mayurbhai24@users.noreply.github.com>
2022-03-27 23:09:24 -0400

    Lab 6

---
+++ scripts/d3.js
@@ -0,0 +1,135 @@
+var svg = d3.select('svg');
+var chart = svg.append('g');
+var grades = ["A","B","C","D","F"]
+
+var data = [];
+
+const width = 800;
+const height = 500;
+const margin = 50;
+const chartWidth = width - 2*margin; // 700
+const chartHeight = height -2*margin; // 400
+
+
+function makeChart(marks) {
+    $("svg").remove();
+
+    createMap(marks);
+
+    const colourScale = d3.scaleLinear()
+                            .domain([978, 2188])
+                            .range(['blue', 'blue']);
+
+    const xScale = d3.scaleBand() // discrete, bucket
+                    .domain(data.map((dat) => dat.mark))
+                    .range([0, chartWidth])
+                    .padding(0.3);
+
+    const yScale = d3.scaleLinear()
+                    .domain([0, 0.5])
+                    .range([chartHeight, 0]);
+
+    let svg = d3.select('body')
+                    .append('svg')
+                    .attr('width', width)
+                    .attr('height', height);
+
+    // title
+    svg.append('text')
+            .attr('x', width / 2)
+            .attr('y', margin)
+            .attr('text-anchor', 'middle')
+            .text('Grade Distribution');
+
+    // create a group (g) for the bars
+    let g = svg.append('g')
+                    .attr('transform', `translate(${margin}, ${margin})`);
+                    
+    // x-axis
+    g.append('g')
+        .attr('transform', `translate(0, ${chartHeight})`)
+        .call(d3.axisBottom(xScale));
+
+    // y-axis
+    g.append('g')
+        .call(d3.axisLeft(yScale));
+    
+
+    let rectangles = g.selectAll('rect')
+    .data(data)
+    .enter()
+        .append('rect')
+            .attr('x', (data) => xScale(data.mark))
+            .attr('y', (data) => chartHeight)
+            .attr('width', xScale.bandwidth())
+            .attr('height', (data) => 0)
+            .attr('fill', (data) => colourScale(data.freq))
+            .on('mouseenter', function(source, index) {
+                d3.select(this)
+                    .transition()
+                    .duration(200)
+                    .attr('opacity', 0.5);
+            })
+            .on('mouseleave', function(source, index) {
+                d3.select(this)
+                    .transition()
+                    .duration(200)
+                    .attr('opacity', 1.0);
+            });
+    
+    rectangles.transition()
+        .ease(d3.easeElastic)
+        .attr('height', (data) => chartHeight - yScale(data.freq))
+        .attr('y', (data) => yScale(data.freq))
+        .duration(1000)
+        .delay((data, index) => index * 50);
+}
+
+function createMap(marks) {
+    data = [];
+    var aCount = 0;
+    var bCount = 0;
+    var cCount = 0;
+    var dCount = 0;
+    var fCount = 0;
+
+    marks.forEach(mark => {
+        letterGrade = getGrade(mark);
+        if(letterGrade == 'A'){
+            aCount++;
+        } else if (letterGrade == 'B'){
+            bCount++;
+        } else if (letterGrade == 'C'){
+            cCount++;
+        } else if (letterGrade == 'D'){
+            dCount++;
+        } else if (letterGrade == 'F'){
+            fCount++;
+        }
+    });
+
+    data = [{"mark": 'A', "freq": (aCount/marks.length)},
+            {"mark": 'B', "freq": (bCount/marks.length)},
+            {"mark": 'C', "freq": (cCount/marks.length)},
+            {"mark": 'D', "freq": (dCount/marks.length)},
+            {"mark": 'F', "freq": (fCount/marks.length)},]
+}
+
+function getGrade(mark) {
+
+    if(mark > 10){
+        mark = mark * 0.1
+    }
+
+    if (mark < 5.0) {
+        return 'F';
+    } else if (mark < 6.0) {
+        return 'D';
+    } else if (mark < 7.0) {
+        return 'C';
+    } else if (mark < 8.0) {
+        return 'B';
+    } else {
+        return 'A';
+    }
+}
--- scripts/spreadsheet.js
+++ scripts/spreadsheet.js
@@ -1,16 +1,41 @@
-var tempArray;
+var itemsArray = new Array(); 
 
 $(document).ready(function() {
     $(function (){
       $('th#Row').click(function() {
-        $(this).siblings().toggleClass('select');
+        var row = $(this).index();
+        itemsArray = $(this).siblings().toggleClass('select').map(function() {
+          return $(this).text();
+      }).get();
+      makeChart(itemsArray);
+      });
+    });
+
+    $(function (){
+      $('td').click(function () {
+        var $this = $(this);
+        var $input = $('<input>', {
+        value: $this.text(),
+         type: 'text',
+         blur: function() {
+            $this.text(this.value);
+         },
+         keyup: function(e) {
+            if (e.which === 13) $input.blur();
+         }
+     }).appendTo( $this.empty() ).focus();
+        $(this).toggleClass('select');
       });
     });
   
     $(function () {
       $('th#Col').click(function () {
         var col = $(this).index();
-            $("td").filter(":nth-child(" + (col + 1) + ")").toggleClass('select');
+            itemsArray =  $("td").filter(":nth-child(" + (col + 1) + ")").toggleClass('select').map(function() {
+              return $(this).text();
+          }).get();
+          makeChart(itemsArray);
+            console.log(itemsArray);
       });
     });
   });
